7d2049847c4355c2a2d183b88375e322f5fc8f98
matoal <may_tove2@hotmail.com>
2020-02-25 10:35:58 +0100

    insert

---
+++ kap_17/tabell_med_FK/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="no" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Legge til et nytt dyr</title>
+  </head>
+  <body>
+    <form action="insert.html" method="post">
+      Navn: <input type="text" name="navn"><br>
+      Art: <input type="text" name="art"><br>
+      Fødselsår: <input type="text" name="aar"><br>
+      Eier_id: <input type="text" name="eier_id"><br>
+      <input type="submit" name="registrer" value="Registrer dyr">
+    </form>
+  </body>
+</html>
